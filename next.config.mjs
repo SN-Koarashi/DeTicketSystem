abdfe4c628627f86728b0240b23895ae80a3bc65
5026 <40320172+SN-Koarashi@users.noreply.github.com>
2025-09-27 06:50:12 +0000

    更新 next 設定

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    trailingSlash: true,
+    devIndicators: { position: "top-left" },
+    env: {
 
+    }
+};
+
+// export default nextConfig;
 export default nextConfig;
+
+// added by create cloudflare to enable calling `getCloudflareContext()` in `next dev`
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+initOpenNextCloudflareForDev();
